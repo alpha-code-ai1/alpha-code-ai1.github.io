2d945c9ba9e74b8eaed74bf8fe321dc58e916af4
gotech123 <gaminginfinity899@gmail.com>
2025-01-08 19:11:31 +0530

    B&W

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1549768686"
+    serviceWorkerVersion: "4284701085"
   }
 });
